99d3db7b75842d553a08ce4d617ba0c6cf2061ae
hansadams <hansadams15@gmail.com>
2024-01-16 04:42:50 +0800

    final v4

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function importAll(r) {
-    return r.keys().map(r);
-  }
-  
-  const images = importAll(require.context('./', false, /\.(png|jpe?g|svg)$/));
-
 root.render(
 <App />
 );
